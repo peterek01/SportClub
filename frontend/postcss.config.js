f60e7be9700597d6ba9786c25a9d03348fa74c3c
peterek01 <135986425+peterek01@users.noreply.github.com>
2025-04-28 22:06:30 +0200

    Update vite.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,5 +1,6 @@
-// frontend/postcss.config.js
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
-};
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: Number(process.env.PORT),
+  },
+  plugins: [vercel()],
+});
